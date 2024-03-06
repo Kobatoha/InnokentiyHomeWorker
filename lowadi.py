af04e25c5338b55509a58bfc12de0ca23651b1e7
Alexandra <kobatoha@gmail.com>
2024-03-06 22:37:45 +0300

    update

--- lowadi.py
+++ lowadi.py
@@ -6,6 +6,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import json
+from os import getenv
 
 
 def newDR():
@@ -26,8 +27,8 @@ def login_lowadi():
     driver.find_element(By.XPATH, '//*[@id="65e8b87c774cfaccept"]/button').click()
 
     driver.find_element(By.XPATH, '//*[@id="header-login-label"]').click()
-    login = driver.find_element(By.XPATH, '//*[@id="login"]').send_keys('Kolgotki')
-    password = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('Sirok123')
+    login = driver.find_element(By.XPATH, '//*[@id="login"]').send_keys(getenv("login"))
+    password = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(getenv("password"))
     connect = driver.find_element(By.XPATH, '//*[@id="authentificationSubmit"]').click()
     try:
         if driver.find_element(By.XPATH, '//*[@id="Ufo_0"]'):
@@ -50,7 +51,21 @@ def work_hourse():
     carrot = driver.find_element(By.XPATH, '//*[@id="boutonCarotte"]').click()
     mash = driver.find_element(By.XPATH, '//*[@id="boutonMash"]').click()
     pat = driver.find_element(By.XPATH, '//*[@id="boutonCaresser"]').click()
-    
+    water = driver.find_element(By.XPATH, '//*[@id="boutonBoire"]').click()
+    food = driver.find_element(By.XPATH, '//*[@id="boutonNourrir"]').click()
+    choice_food = driver.find_element(By.XPATH, '//*[@id="haySlider"]/ol/li[13]').click()
+    get_food = driver.find_element(By.XPATH, '//*[@id="feed-button"]').click()
+    sleep = driver.find_element(By.XPATH, '//*[@id="boutonCoucher"]').click()
+    lesson = driver.find_element(By.XPATH, '//*[@id="mission-tab-0"]/div/div/div[2]').click()
+    driver.find_element(By.XPATH, '//*[@id="title-genetics"]').click()
+    gp = int(driver.find_element(By.XPATH, '//*[@id="genetic-body-content"]/table[1]/tbody/tr[1]/td[3]').text[6:-3])
+    if gp > 18000:
+        print(f'Хороший ГП, можно качать')
+    next_hourse = driver.find_element(By.XPATH, '//*[@id="nav-next"]').click()
+
+
+
+
 
 
 driver = newDR()
