68262f1f8f40907bab677fe29eaac4765926ba21
Kanian Tai <166604509+EitheryMJ@users.noreply.github.com>
2024-05-07 14:07:00 +0300

    Create chrome_driver.py
    comming out undetected_chromedriver in another module

---
+++ chrome_driver.py
@@ -0,0 +1,19 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver import Keys, ActionChains
+
+extension = 'omghfjlpggmjjaagoclmmobgdodcjboh'
+
+
+def newDRB(): 
+    chromeOptions = uc.ChromeOptions()
+    caps = DesiredCapabilities().CHROME
+    caps["pageLoadStrategy"] = "eager"
+    chromeOptions.add_argument(
+        "--load-extension=" +
+        rf"C:\Users\aleksandra.usupova\AppData\Local\Google\Chrome\User Data\Default\Extensions\{extension}\3.84.1_0")
+    chromeOptions.headless = False
+    driver = uc.Chrome(options=chromeOptions, desired_capabilities=caps)
+
+    return driver
