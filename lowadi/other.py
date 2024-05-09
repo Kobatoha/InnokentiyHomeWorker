6e3f7eabbc39fc2a023cb7da15106d18eeb35e6c
Alexandra <kobatoha@gmail.com>
2024-05-09 23:07:13 +0300

    update

---
+++ lowadi/other.py
@@ -0,0 +1,226 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver import Keys, ActionChains
+import os
+import random
+from datetime import datetime
+import time
+from chrome_driver import newDRB
+
+
+def check_ufo():
+
+    good_ufo = {'vieillissement': 'очки роста'}
+    bad_ufo = {'pomme': 'яблоко', 'carotte': 'морковь', 'classique': 'седло', 'mash': 'комбикорм',
+               'equus': 'экю'}
+    try:
+
+        if driver.find_element(By.XPATH, '//*[@id="Ufo_0"]'):
+            icon = driver.find_element(By.XPATH, '/html/body/div[1]/img').get_attribute('src')
+
+            for bad_key in bad_ufo:
+                if bad_key in icon:
+                    print(f"НЛО: {bad_ufo[bad_key]}, надо скипнуть")
+                    driver.refresh()
+
+            for good_key in good_ufo:
+                if good_key in icon:
+                    print(f"НЛО: {good_ufo[good_key]}, надо брать")
+                    driver.find_element(By.XPATH, '//*[@id="Ufo_0"]').click()
+                    time.sleep(1)
+                    driver.find_element(By.XPATH, '//*[@id="agi-10972849001709751854"]').click()
+                    time.sleep(1)
+                    driver.find_element(By.XPATH, '/html/body/div[12]/aside/div[2]/button/span/span').click()
+                    driver.refresh()
+                    time.sleep(1)
+
+            driver.refresh()
+            time.sleep(1)
+
+    except:
+
+        return
+
+
+def login_lowadi():
+    now = datetime.now().strftime('%H:%M:%S')
+    print(f'{now}: Логинимся на lowadi.com')
+    url = 'https://www.lowadi.com'
+
+    driver.get(url)
+
+    try:
+        print('Принимаем cookie')
+        driver.find_element(By.XPATH, '/html/body/aside/div/article/div/div[2]/div/div/div[3]/form/button').click()
+
+    except:
+        print('Принять cookie не просили')
+
+    try:
+
+        print(f'Начинаем!')
+        driver.find_element(By.XPATH, '/html/body/div[7]/header/nav/div/strong').click()
+        time.sleep(2)
+        if driver.find_element(By.XPATH, '//*[@id="login"]').text == '':
+            print(f'Вводим логин..')
+            login = driver.find_element(By.XPATH, '//*[@id="login"]').send_keys('Kolgotki')
+            time.sleep(1)
+            print(f'Вводим пароль..')
+            password = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('Sirok123')
+            time.sleep(1)
+        print(f'Входим')
+        connect = driver.find_element(
+            By.XPATH,
+            '/html/body/div[7]/header/nav/div/div/form/button/span/span/span'
+        ).click()
+        time.sleep(1)
+
+    except:
+
+        url = 'https://www.lowadi.com/elevage/chevaux/cheval?id=20378769'
+        driver.get(url)
+
+        print(f'Пробуем зайти заново')
+        time.sleep(10)
+        if driver.find_element(By.XPATH, '//*[@id="login"]').text == '':
+            print(f'Вводим логин..')
+            login = driver.find_element(By.XPATH, '//*[@id="login"]').send_keys('Kolgotki')
+            time.sleep(1)
+            print(f'Вводим пароль..')
+            password = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('Sirok123')
+            time.sleep(1)
+            print(f'Входим')
+        connect = driver.find_element(
+            By.XPATH,
+            '/html/body/div[7]/main/section/section/aside/form/button'
+        ).click()
+        time.sleep(10)
+
+
+def xanthos():
+    """
+    Ежедневная глажка 5 Ксанфов.
+    Есть вероятность получить рог изобилия.
+    :return:
+    """
+    urls = [
+        'https://www.lowadi.com/elevage/fiche/?id=1045736',
+        'https://www.lowadi.com/elevage/fiche/?id=1047131',
+        'https://www.lowadi.com/elevage/fiche/?id=1044747',
+        'https://www.lowadi.com/elevage/fiche/?id=1044960',
+        'https://www.lowadi.com/elevage/fiche/?id=1046269'
+    ]
+    for url in urls:
+        driver.get(url)
+        time.sleep(2)
+        pat_xanthos = driver.find_element(
+            By.XPATH,
+            '/html/body/div[7]/main/section/section/div/div/div[1]/div[2]'
+            '/div/div/div/div/div/div[1]/table/tbody/tr/td[3]/form/a'
+        ).click()
+        time.sleep(2)
+
+
+def topaz():
+    """
+    Ежедневная глажка 5 Топазов.
+    Есть вероятность получить 10 пропусков.
+    :return:
+    """
+    urls = [
+        'https://www.lowadi.com/elevage/fiche/?id=4763736',
+        'https://www.lowadi.com/elevage/fiche/?id=4775333',
+        'https://www.lowadi.com/elevage/fiche/?id=4771120',
+        'https://www.lowadi.com/elevage/fiche/?id=4792032',
+        'https://www.lowadi.com/elevage/fiche/?id=4789329'
+    ]
+    for url in urls:
+        driver.get(url)
+        time.sleep(2)
+        pat_topaz = driver.find_element(
+            By.XPATH,
+            '/html/body/div[7]/main/section/section/div/div/div[1]/div[2]'
+            '/div/div/div/div/div/div[1]/table/tbody/tr/td[3]/form/a'
+        ).click()
+        time.sleep(2)
+
+
+def givre():
+    """
+    Ежедневная разморозка Морозницы.
+    Есть вероятность получить Одеяло Гипноса.
+    :return:
+    """
+    urls = [
+        'https://www.lowadi.com/elevage/fiche/?id=12086505'
+    ]
+    for url in urls:
+        driver.get(url)
+        time.sleep(2)
+        pat_givre = driver.find_element(
+            By.XPATH,
+            '/html/body/div[7]/main/section/section/div/div/div[1]/div[2]/'
+            'div/div/div/div/div/div[1]/table/tbody/tr/td[3]/a'
+        ).click()
+        time.sleep(2)
+
+
+def atelier():
+    print(datetime.now().strftime('%H:%M:%S'), 'Проверяем мастерские..')
+    url = 'https://www.lowadi.com/centre/atelier/'
+    driver.get(url)
+    try:
+        for _ in range(2):
+            try:
+                get_stable_1 = driver.find_element(
+                    By.XPATH,
+                    '/html/body/div[7]/main/section/section/form/table/tbody/tr[3]/td[6]/a/span'
+                )
+            except:
+                get_stable_1 = driver.find_element(
+                    By.XPATH,
+                    '/html/body/div[8]/main/section/section/form/table/tbody/tr[3]/td[6]/a/span'
+                )
+            match get_stable_1.text:
+                case 'Получить':
+                    print('Стойла готовы, собираем')
+                    get_stable_1.click()
+                    time.sleep(2)
+                    try:
+                        driver.find_element(
+                            By.XPATH,
+                            '/html/body/div[11]/div/div/form/button/span/span/span'
+                        ).click()
+                    except:
+                        driver.find_element(
+                            By.XPATH,
+                            '/html/body/div[12]/div/div/form/button/span/span/span'
+                        ).click()
+                    time.sleep(2)
+                case 'Стоп':
+                    print('Стойла не готовы, сваливаем')
+                    return 0
+
+        for _ in range(2):
+            begin_stable = driver.find_element(
+                By.XPATH,
+                '/html/body/div[8]/main/section/section/form/table/tbody/tr[1]/td[6]/button/span/span/span'
+            )
+            match begin_stable.text:
+                case 'Производство':
+                    print('Мастерская свободна, пора запускать постройку стойла')
+                    begin_stable.click()
+                    time.sleep(2)
+                    driver.find_element(
+                        By.XPATH,
+                        '/html/body/div[12]/div/div/div/div/div/div/div[1]/div/div[3]/div[1]/div/span'
+                    ).click()
+                    time.sleep(2)
+                    driver.find_element(
+                        By.XPATH,
+                        '/html/body/div[12]/div/div/div/div/div/div/div[2]/form[8]/div[2]/button/span/span/span'
+                    ).click()
+                    time.sleep(2)
+
+    except:
+        print('Проверка мастерской провалилась')
