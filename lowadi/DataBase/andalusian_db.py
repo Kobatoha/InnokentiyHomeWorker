ed9a35f51464b45058c3f7a7948a412581fe2f78
Alexandra <kobatoha@gmail.com>
2024-06-04 21:04:20 +0300

    create insert func

--- lowadi/DataBase/andalusian_db.py
+++ lowadi/DataBase/andalusian_db.py
@@ -2,14 +2,13 @@ import duckdb
 
 
 def create_connection(db_file):
-    connect = duckdb.connect(database=db_file)
+    connect = duckdb.connect(database=database)
     return connect
 
 
 def create_table(connect):
     create_table_duck = """
     CREATE TABLE IF NOT EXISTS andalusian (
-        id INTEGER PRIMARY KEY,
         name VARCHAR,
         birthday DATE,
         sex TINYINT,
@@ -20,13 +19,28 @@ def create_table(connect):
         dressage BOOLEAN,
         galop BOOLEAN,
         forest BOOLEAN,
-        montains BOOLEAN
+        montains BOOLEAN,
+        url VARCHAR
     );
     """
     conn.execute(create_table_duck)
 
-# Подключаемся к базе данных и создаем таблицу
-db_file = 'example.duckdb'
-conn = create_connection(db_file)
-create_table(conn)
-conn.close()
+
+def insert_table(connect, data: list) -> None:
+    query = f"""
+    insert into andalusian 
+    (name, birthday, sex, color, rare, armor, speed, dressage, galop, forest, montains, url) 
+    values 
+    ({data[0]}, {data[1]}, {data[2]}, {data[3]}, {data[4]}, {data[5]}, {data[6]}, {data[7]}, {data[8]}, {data[9]}, 
+    {data[10]}, {data[11]})
+    """
+    connect.execute(query)
+    connect.commit()
+    print(f'INSERT: {data[0]} in andalusian team')
+
+
+if __name__ == '__main__':
+    database = r'C:\Users\Admin\Desktop\python\Innokentiy\lowadi\DataBase\andalusian.duckdb'
+    conn = create_connection(database)
+    create_table(conn)
+    conn.close()
