13940eddeeca46cb053af75710644ee979e227b4
kobatoha <kobatoha@gmail.com>
2024-07-22 16:35:14 +0300

    update

--- lowadi.py
+++ lowadi.py
@@ -16,23 +16,12 @@ def begin(driver):
     time.sleep(5)
 
 
-def second_step(driver):
-    xanthos(driver)
-    time.sleep(5)
-    topaz(driver)
-    time.sleep(5)
-    givre(driver)
-    time.sleep(5)
-    atelier(driver)
-    time.sleep(5)
-
-
 if __name__ == '__main__':
     try:
 
         print(f'{datetime.now().strftime("%H:%M:%S")}: запускаем chrome')
         driver = new_drb()
-        driver.set_window_size(1900, 1000)
+        driver.set_window_size(1400, 1300)
 
     except:
 
@@ -42,12 +31,12 @@ if __name__ == '__main__':
         login_lowadi(driver)
         begin(driver)
 
-
     login_lowadi(driver)
     work_heavyhorse(driver, 100)
     work_female(driver, 2055)
     work_male(driver, 200)
 
     """
-    75 коней с гп 15к+ = 300 ежедневных случек
+    75 коней с гп 15к+ = 4 х 75 = 300 ежедневных случек = 75 душевых и поилок = х2 набора случки\кач
+    80 лугов нужно под 2000 стойл = 160 на два набора, пока один под пастбища, другой восстанавливает плодородность
     """
