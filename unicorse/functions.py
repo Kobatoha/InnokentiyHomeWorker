3386408c5d112b9e21a8fd54118e6a12b7ce3d52
kobatoha <kobatoha@gmail.com>
2024-07-28 00:44:07 +0300

    update all buttons func

--- unicorse/functions.py
+++ unicorse/functions.py
@@ -1,13 +1,33 @@
-def handle_btn1():
-    print('Кнопка 1 нажата!')
+def handle_driver_brave():
+    print('Кнопка driver_brave нажата!')
 
 
-def handle_btn2():
-    print('Кнопка 2 нажата!')
+def handle_driver_chrome():
+    print('Кнопка driver_chrome нажата!')
 
 
-def handle_btn3():
-    print('Кнопка 3 нажата!')
+def handle_female_andalus():
+    print('Кнопка female_andalus нажата!')
+
+
+def handle_male_andalus():
+    print('Кнопка male_andalus нажата!')
+
+
+def handle_unicorn_andalus():
+    print('Кнопка unicorn_andalus нажата!')
+
+
+def handle_competitions_andalus():
+    print('Кнопка competitions_andalus нажата!')
+
+
+def handle_blup_andalus():
+    print('Кнопка blup_andalus нажата!')
+
+
+def handle_female_marshadore():
+    print('Кнопка female_marshadore нажата!')
 
 
 def count_horses():
