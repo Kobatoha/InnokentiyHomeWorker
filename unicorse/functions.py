2340744d29f39782e2d0f1ff91902c8ea9ddce62
kobatoha <kobatoha@gmail.com>
2024-07-28 00:29:26 +0300

    add functions pass

---
+++ unicorse/functions.py
@@ -0,0 +1,14 @@
+def handle_btn1():
+    print('Кнопка 1 нажата!')
+
+
+def handle_btn2():
+    print('Кнопка 2 нажата!')
+
+
+def handle_btn3():
+    print('Кнопка 3 нажата!')
+
+
+def count_horses():
+    return 1
