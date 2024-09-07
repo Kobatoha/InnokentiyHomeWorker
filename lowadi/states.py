72af8eca005ef63ed6dda98042e092d405d53d79
kobatoha <kobatoha@gmail.com>
2024-09-07 14:08:53 +0300

    init states

---
+++ lowadi/states.py
@@ -0,0 +1,12 @@
+driver_chrome_state = False
+driver_brave_state = False
+
+login_state = False
+begin_state = False
+
+andalusian_female_state = False
+andalusian_elite_female_state = False
+andalusian_male_state = False
+andalusian_male_reserve_state = False
+marshadore_female_state = False
+marshadore_male_state = False
