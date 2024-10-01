57cf1c0057e1e4bdf4f6a8f3ae853630a084aba4
kobatoha <kobatoha@gmail.com>
2024-10-01 19:57:00 +0300

    update telegram funcs

--- main.py
+++ main.py
@@ -1,40 +1,80 @@
-import asyncio
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from config_reader import config
+import os
+import requests
+import datetime
 from lowadi import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
 
-bot = Bot(token=config.bot_token.get_secret_value())
+LESSONS_TODAY = [
+    '08:00 - Математика',
+    '09:00 - Английский язык',
+    '10:00 - Химия',
+    '11:00 - История',
+    '12:00 - Физика'
+]
 
-logging.basicConfig(level=logging.INFO)
-# Объект бота
-bot = Bot(token=TOKEN)
-# Диспетчер
-dp = Dispatcher()
-
-
-# Хэндлер на команду /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
-
-
-@dp.message(Command("test1"))
-async def cmd_test1(message: types.Message):
-    await message.answer("Test 1")
+LESSONS_TOMORROW = [
+    '08:00 - Биология',
+    '09:00 - География',
+    '10:00 - Литература',
+    '11:00 - Информатика',
+    '12:00 - Физкультура'
+]
 
 
-@dp.message(Command("test2"))
-async def cmd_test1(message: types.Message):
-    await message.answer("Test 2")
+def get_updates():
+    """Функция для получения обновлений (сообщений) от Telegram API"""
+    url = BASE_URL + 'getUpdates'
+    response = requests.get(url)
+    return response.json()
 
 
-# Запуск процесса поллинга новых апдейтов
-async def main():
-    await dp.start_polling(bot)
+def send_message(chat_id, text):
+    """Отправка сообщения пользователю"""
+    url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={text}'
+    requests.get(url)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+def get_lesson_schedule():
+    """Получить расписание в зависимости от времени запроса"""
+    current_time = datetime.datetime.now().time()
+    # Сравниваем текущее время с 08:00
+    if current_time < datetime.time(8, 0):
+        return 'Сегодняшние уроки:\n' + '\n'.join(LESSONS_TODAY)
+    else:
+        return 'Завтрашние уроки:\n' + '\n'.join(LESSONS_TOMORROW)
+
+
+def handle_message(message):
+    """Обработка входящего сообщения"""
+    chat_id = message['chat']['id']
+    text = message['text']
+
+    if text == '/lessons':
+        lesson_schedule = get_lesson_schedule()
+        send_message(chat_id, lesson_schedule)
+    else:
+        send_message(chat_id, 'Команда не распознана. Используйте /lessons для получения расписания.')
+
+
+def main():
+    last_update_id = None
+
+    while True:
+        updates = get_updates()
+
+        if updates['ok']:
+            for update in updates['result']:
+                # Обрабатываем только новые сообщения
+                if last_update_id is None or update['update_id'] > last_update_id:
+                    last_update_id = update['update_id']
+                    handle_message(update['message'])
+
+
+if __name__ == '__main__':
+    main()
