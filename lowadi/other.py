b9360c19878c80e2daf8b269228acb011b2482f3
kobatoha <kobatoha@gmail.com>
2024-10-06 10:25:25 +0300

    update

--- lowadi/other.py
+++ lowadi/other.py
@@ -320,7 +320,7 @@ def get_resurses(driver, name):
     return resurses[name]
 
 
-def spend_equus(driver, res='skin'):
+def spend_equus(driver, res='wood'):
     try:
         equus = int(driver.find_element(
             By.XPATH,
