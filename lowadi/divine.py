6232acab985195482c1168975f2876e5d3d31c22
kobatoha <kobatoha@gmail.com>
2024-10-30 12:22:50 +0300

    update

--- lowadi/divine.py
+++ lowadi/divine.py
@@ -1,3 +1,5 @@
+import time
+
 from lowadi.care import *
 
 
@@ -7,5 +9,6 @@ def tianma(driver):
     driver.get(url)
     get_lesson(driver)
     get_doping(driver)[-1].click()
+    time.sleep(1)
     get_food(driver)
     get_sleep(driver)
